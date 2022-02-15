b771ea6d5177d5abb266d3bb3387131ed56a3081
Vuk Podinic <vukpodinic@Vuks-MacBook-Pro.local>
2022-02-15 23:01:46 +1100

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -143,24 +143,6 @@ export default function Example() {
           </div>
         </div>
       </div>
-
-      <div className="bg-blue-700 max-w-7xl mx-auto rounded-md">
-        <div className="max-w-2xl mx-auto text-center py-16 px-4 sm:py-20 sm:px-6 lg:px-8">
-          <h2 className="text-3xl font-extrabold text-white sm:text-4xl">
-            <span className="block">Let's have a coffee.</span>
-          </h2>
-          <p className="mt-4 text-lg leading-6 text-blue-200">
-            Send me an email rajko.podinic@gmail.com
-          </p>
-          <a
-            href="#"
-            className="mt-8 w-full inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-blue-600 bg-white hover:bg-blue-50 sm:w-auto"
-          >
-            Sign up for free
-          </a>
-        </div>
-      </div>
-
     </>
   )
 }
